dc5a9de518d518b59dd261107c7a414c8f5aa915
putrialutfi <putrilutfiani29@gmail.com>
2021-09-26 16:58:41 +0700

    date standardizer v1

---
+++ Date Standardizer/README.md
@@ -0,0 +1,41 @@
+## Date standardizer for Bahasa Indonesia
+
+Simple code for date standardization for Bahasa Indonesia format. 
+The input must be in Bahasa Indonesia date of pattern (day-month-year). But, it works for with every format of the date. 
+
+# libraries
+<ul>
+    <li>dateutil, for standardization the date.</li>
+    <li>difflib, for handle the month name typo and convert it to English month name.</li>
+</ul>
+
+
+# usage
+write the date using Bahasa Indonesia date pattern (day-month-year) in any format. ex: 
+<ul>
+    <li>28 juni 1987</li>
+    <li>dua november seribu sembilan ratus sembilan puluh lima</li>
+    <li>01-02-2010</li>
+</ul>
+
+
+## Standarisasi Tanggal untuk Bahasa Indonesia
+
+Kode sederhana untuk standarisasi tanggal dalam format Bahasa Indonesia.
+Input harus dalam pola tanggal Bahasa Indonesia (hari/tanggal-bulan-tahun). Tapi, ini bisa digunakan di setiap format tanggal.
+
+
+# libraries
+<ul>
+    <li>dateutil, for standarisasi tanggal.</li>
+    <li>difflib, untuk menangani kesalahan penulisan nama bulan dan mengkonversi nama bulan berbahasa Indonesia menjadi nama bulan dalam Bahasa Inggris. Library dateutil hanya bekerja jika nama bulan berbahasa Inggris.</li>
+</ul>
+
+
+# usage
+tulis tanggal menggunakan pola tanggal dalam Bahasa Indoeneisa (hari/tanggal-bulan-tahun) menggunakan format apapun. Contoh: 
+<ul>
+    <li>28 juni 1987</li>
+    <li>dua november seribu sembilan ratus sembilan puluh lima</li>
+    <li>01-02-2010</li>
+</ul>
---
+++ Date Standardizer/date_standardizer.py
@@ -0,0 +1,205 @@
+import difflib
+from dateutil import parser
+from dateutil.tz import gettz
+
+date_format = "%d/%m/%Y" #adjustable
+
+months = {
+    'januari': 'january', 
+    'februari': 'february', 
+    'maret': 'march', 
+    'april': 'april', 
+    'mei': 'may', 
+    'juni': 'june', 
+    'juli': 'july', 
+    'agustus': 'august', 
+    'september': 'september', 
+    'oktober': 'october', 
+    'november': 'november', 
+    'desember': 'december'
+  }
+
+
+nums = {
+    'satu': {
+        'num': 1,
+        'op': None,
+        'type': 'base'
+    },
+    'dua': {
+        'num': 2,
+        'op': None,
+        'type': 'base'
+    },
+    'tiga': {
+        'num': 3,
+        'op': None,
+        'type': 'base'
+    },
+    'empat': {
+        'num': 4,
+        'op': None,
+        'type': 'base'
+    },
+    'lima': {
+        'num': 5,
+        'op': None,
+        'type': 'base'
+    },
+    'enam': {
+        'num': 6,
+        'op': None,
+        'type': 'base'
+    },
+    'tujuh': {
+        'num': 7,
+        'op': None,
+        'type': 'base'
+    },
+    'delapan': {
+        'num': 8,
+        'op': None,
+        'type': 'base'
+    }, 
+    'sembilan': {
+        'num': 9,
+        'op': None,
+        'type': 'base'
+    },
+    'sepuluh': {
+        'num': 10,
+        'op': None,
+        'type': 'base'
+    },
+    'sebelas': {
+        'num': 11,
+        'op': None,
+        'type': 'base'
+    },
+    'seratus': {
+        'num': 100,
+        'op': None,
+        'type': 'base'
+    },
+    'seribu': {
+        'num': 1000,
+        'op': None,
+        'type': 'base'
+    },
+
+    'belas': {
+        'num': 10,
+        'op': 'add',
+        'type': 'sym'
+    },
+    'puluh': {
+        'num': 10,
+        'op': 'multiple',
+        'type': 'sym'
+    },
+    'ratus': {
+        'num': 100,
+        'op': 'multiple',
+        'type': 'sym'
+    },
+    'ribu': {
+        'num': 1000,
+        'op': 'multiple',
+        'type': 'sym'
+    }
+}
+
+
+
+
+# find the highest score 
+def month_corrector(mo_token):
+  higest = 0
+  fix_mo = ''
+  for key, val in months.items():
+    seq = difflib.SequenceMatcher(None, mo_token, key)
+    seq = seq.quick_ratio()*100
+    if seq > higest:
+      higest = seq
+      fix_mo = val
+
+  return fix_mo, higest
+
+
+
+def str_to_num(strnum):
+  res, temp, temp2 = 0, 0, 0
+
+  strdate = strnum.split()
+  dlist = [i for i in range(len(strdate))]
+  for idx, d in enumerate(strdate):
+    for key, val in nums.items():
+      if d == key and val.get('type') == 'sym':
+        temp = nums.get(d).get('num') * nums.get(strdate[idx-1]).get('num') if val.get('op') == 'multiple' else nums.get(d).get('num') + nums.get(strdate[idx-1]).get('num')
+        res = res + temp
+        dlist = [x for x in dlist if (x not in [idx, idx-1])]
+  for rest in dlist:
+    temp2 = nums.get(strdate[rest]).get('num')
+    res = res + temp2
+
+  return res
+
+
+
+
+def date_to_num(date_token):
+  datestring = ' '.join([i for i in date_token])
+
+  hscore = 0
+  for month in date_token:
+    mo, score = month_corrector(month) 
+    if score > hscore:
+      hmonth = month.lower()
+      hscore = score
+
+  day = datestring.lower().split(hmonth)[0]
+  year = datestring.lower().split(hmonth)[1]
+
+  day_num = str_to_num(day)
+  year_num = str_to_num(year)
+
+  return str(day_num), hmonth, str(year_num)
+
+
+
+
+# the core code
+def date_standardizer(date_token):
+  date_token = date_token.split()
+  
+  if len(date_token) > 2:
+    if any(i.isdigit() for i in date_token):
+      day_token = date_token[0].lower()
+      mo_token = date_token[1] if date_token[1].lower().isnumeric() else month_corrector(date_token[1].lower())[0]
+      year_token = date_token[2].lower()
+      
+      date_eng_format = day_token + ' ' + mo_token + ' ' + year_token
+    
+    else:
+      day, month, year = date_to_num(date_token)
+      date_eng_format = str(day) + " " + month_corrector(month)[0] + " " + str(year)
+  else:
+    date_eng_format = date_token[0]
+
+  tzinfos = {"ICT": gettz("Asia/Jakarta")}
+  final_date = parser.parse(date_eng_format, 
+                            tzinfos=tzinfos, 
+                            dayfirst=True).strftime("%d/%m/%Y")
+
+  return final_date
+
+
+print("Pola tanggal berbahasa indonesia hari-bulan-tahun, format bebas, contoh: ")
+print("-28 juni 1987")
+print("-dua november seribu sembilan ratus sembilan puluh lima")
+
+
+date_input = input("\nMasukkan input tanggal: ")
+
+print("-"*20)
+print("Hasil standarisasi: ", date_standardizer(date_input))
